7dca1c9973c6bc1fe891f9386eff02cfa16b98d5
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-12-27 21:48:47 -0500

    Added non bootstrapped Nav components while figuring out mbdreact

---
+++ growingup/src/components/Nav/NavTop/NavTop.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NavTop.css";
+
+const NavTop = () => (
+  <nav className="navbar navbar-dark bg-dark">
+    This is a NON bootstrapped Nav Component
+    <a className="navbar-brand" href="/">
+      This is a NON bootstrapped Nav Link  
+    </a>
+  </nav>
+);
+
+export default NavTop;
