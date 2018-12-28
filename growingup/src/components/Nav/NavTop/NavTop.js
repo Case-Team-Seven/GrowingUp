675289e4fcfbde992345d12b80c612f8f8436bb3
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-12-28 17:15:06 -0500

    Added the topNav Component Successfully

--- growingup/src/components/Nav/NavTop/NavTop.js
+++ growingup/src/components/Nav/NavTop/NavTop.js
@@ -1,13 +1,66 @@
-import React from "react";
-import "./NavTop.css";
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-const NavTop = () => (
-  <nav className="navbar navbar-dark bg-dark">
-    This is a NON bootstrapped Nav Component
-    <a className="navbar-brand" href="/">
-      This is a NON bootstrapped Nav Link  
-    </a>
-  </nav>
-);
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default NavTop;
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
