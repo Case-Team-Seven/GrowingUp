df6141026cd2852aa66bff1330b9d7223d1cc1c4
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2019-01-15 00:17:27 -0500

    added new nav bar and fit things better with new installations

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import NavTop from "./components/Nav/NavTop/NavTop";
+import {NavTop} from "./components/Nav/NavTop/NavTop";
 import LogIn from "./components/LogIn/LogIn";
 import CategoryList from "./components/CategoryList/CategoryList";
 import ForumPost from "./components/Forums/ForumPost";
@@ -9,16 +9,17 @@ import NewPostButton from './components/Button/NewPost/NewPostButton';
 import './App.css';
 import { Button } from 'reactstrap';
 
+
 class App extends Component {
     render() {
         return (
-            <div className="mainContainer" href="/">
-                <div className="header" id="masthead">
-                    <NavTop className="navTop" />
-                    <NewPostButton className="newPostButton"/>
-
-                </div>
-                <div className="logInContainer" href="/">
+            <div className="mainContainer">
+                
+                
+                    <NavTop render={<h1>Grownupish</h1>}/>
+                    <NewPostButton />
+                
+                <div className="logInContainer" >
                     <LogIn />
                 </div>
                 <div className="logInContainer">
--- client/src/components/Nav/NavTop/NavTop.js
+++ client/src/components/Nav/NavTop/NavTop.js
@@ -1,73 +1,111 @@
 import React from 'react';
 import './NavTop.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+// import {
+//   Collapse,
+//   Navbar,
+//   NavbarToggler,
+//   NavbarBrand,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   UncontrolledDropdown,
+//   DropdownToggle,
+//   DropdownMenu,
+//   DropdownItem } from 'reactstrap';
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
+// export default class Example extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
+//     this.toggle = this.toggle.bind(this);
+//     this.state = {
+//       isOpen: false
+//     };
 
-    {/* isOpen prop determines whether the modal should be visible. The toggle callback can be used to toggle the value of isOpen in the controlling component. */}
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
+//     {/* isOpen prop determines whether the modal should be visible. The toggle callback can be used to toggle the value of isOpen in the controlling component. */}
+//   }
+//   toggle() {
+//     this.setState({
+//       isOpen: !this.state.isOpen
+//     });
+//   }
+//   render() {
+//     return (
+//         <div className="navBarTop">
+//           <Navbar  background-color="white" expand="md">
+//             {/* Brandname */}
+//             <NavbarBrand href="/">
+//               Grownupish
+//             </NavbarBrand>
+//             {/* Add toggler to auto-collapse */}
+//             <NavbarToggler onClick={this.toggle} />
+//             <Collapse isOpen={this.state.isOpen} navbar>
+
+//               {/*Pull left */}
+//               <Nav className="ml-auto" navbar>
+//                 <NavItem>
+//                   <NavLink href="/components/">
+//                     Link Option
+//                   </NavLink>
+//                 </NavItem>
+//                 <NavItem>
+//                   <NavLink href="https://github.com/reactstrap/reactstrap">
+//                     GitHub
+//                   </NavLink>
+//                 </NavItem>
+
+
+//                 {/* Pull right */}
+//                 <UncontrolledDropdown nav inNavbar>
+
+//                   <DropdownToggle nav caret>
+//                     Your Profile
+//                   </DropdownToggle>
+
+
+//                   <DropdownMenu right>
+//                     <DropdownItem>
+//                       Option 1 (LogIn / View Your Profile ( When logged in))
+//                     </DropdownItem>
+//                     <DropdownItem>
+//                       Option 2 ("LogOut" (Can be seen only When logged in)
+//                     </DropdownItem>
+//                     <DropdownItem divider />
+//                     <DropdownItem>
+//                       Reset
+//                     </DropdownItem>
+//                   </DropdownMenu>
+//                 </UncontrolledDropdown>
+//               </Nav>
+//             </Collapse>
+//           </Navbar>
+//         </div>
+//     );
+//   }
+// }
+
+
+// import React from 'react';
+
+export class NavTop extends React.Component {
   render() {
-    return (
-        <div className="navBarTop">
-          <Navbar  background-color="white" expand="md">
-            {/* Brandname */}
-            <NavbarBrand href="/">
-              Grownupish
-            </NavbarBrand>
-            {/* Add toggler to auto-collapse */}
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
+    const pages = ['', '','Option 1', 'LogIn/SignUp'];
+    
+      
+    
+    const navLinks = pages.map(page => {
+      return (
+          <div>
+            <div className='navItem'>
+              <a href={'/' + page}>
+                {page}
+              </a>
+            </div>  
+          </div>
+          
+      )
+    });
 
-
-
-                {/* Pull right */}
-                <UncontrolledDropdown nav inNavbar>
-
-                  <DropdownToggle nav caret>
-                    Your Profile
-                  </DropdownToggle>
-
-
-                  <DropdownMenu right>
-                    <DropdownItem>
-                      Option 1 (LogIn / View Your Profile ( When logged in))
-                    </DropdownItem>
-                    <DropdownItem>
-                      Option 2 ("LogOut" (Can be seen only When logged in)
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                      Reset
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </Nav>
-            </Collapse>
-          </Navbar>
-        </div>
-    );
+    return <nav className='navBarTop'>{navLinks}</nav>;
   }
 }
