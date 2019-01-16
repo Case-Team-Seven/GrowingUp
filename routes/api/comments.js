049afe0b7ea876c1d176ebac45678074591b7762
nmiesmer <nic.miesmer@segmint.com>
2019-01-16 00:08:58 -0500

    two more add endpoints

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -31,6 +31,28 @@ router.get("/:id", function(req, res) {
 
 });
 
+// TODO This should be POST_ID not TOPIC_ID
+// api/comments/add
+router.post("/add", function(req, res) {
+
+    const comment = req.body;
+    console.log("REQ.BODY", req.body);
+
+    const sql = "INSERT INTO `comments` (body, user_id, topic_id, created, updated) VALUES (?, ?, ?, ?, ?)"
+
+
+    connection.query(sql, [comment.body, comment.user_id, comment.topic_id,
+        comment.created, comment.updated],  function(err, result){
+        if(err) throw err;
+        console.log("1 record inserted");
+    });
+
+    // res.send(user);
+    res.redirect("/")
+});
+
+
+
 
 
 
