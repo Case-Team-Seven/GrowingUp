7560fabd2f2f88e38f45969af76d9226717ca3c1
Zverloff <toddzverloff@gmail.com>
2019-01-17 19:53:31 -0500

    backend login updated

--- client/src/components/LogIn/LogIn.js
+++ client/src/components/LogIn/LogIn.js
@@ -1,31 +1,63 @@
 import React, { Component } from 'react';
 import { Collapse, Button, CardBody, Card, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { FormControl, ControlLabel } from "react-bootstrap";
+import "./LogIn.css";
 
-class Example extends Component {
-    constructor(props) {
-        super(props);
-        this.toggle = this.toggle.bind(this);
-        this.state = { collapse: false };
-    }
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
-    toggle() {
-        this.setState({ collapse: !this.state.collapse });
-    }
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                {/* <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
-        <Collapse isOpen={this.state.collapse}> */}
-                {/* <Card>
-            <CardBody>
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
 
-            </CardBody>
-          </Card> */}
-                {/* </Collapse> */}
-            </div>
-        );
-    }
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default Example;
