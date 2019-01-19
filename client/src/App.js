65e7b6b09f5a23036af9c2d9bf0b44451bdaeb85
Zverloff <toddzverloff@gmail.com>
2019-01-19 17:56:25 -0500

    starting over

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 
 import './App.css';
@@ -7,53 +6,34 @@ import './App.css';
 // import UserProfile from "./pages/Users/UserProfile"
 // import TopicPosts from "./pages/Posts/TopicPosts"
 
-import './components/CategoryList/CategoryList.css';
+
 import { Button } from 'reactstrap';
-// import Post from './components/Forums/Post';
-// import PostForm from "./pages/PostForm/PostForm";
-//import Home from "./pages/Home/Home";
 
-// import Finance from "./pages/Finance/Finance";
-// import Education from "./pages/Education/Education";
-// import SocialMedia from "./pages/SocialMedia/SocialMedia";
-// import Relationships from "./pages/Relationships/Relationships";
-// import Partying from "./pages/Partying/Partying";
-// import Career from "./pages/Career/Career";
-// import TimeManagement from "./pages/TimeManagement/TimeManagement";
-// import Family from "./pages/Family/Family";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
-const App = () => (
-    <Router>
-        <div>
-
-            <Switch>
-                {/* <Route exact path="/" component={Home} />
-                <Route exact path="/Home" component={Home} /> */}
- 
-                {/* <Route exact path="/addPost" component={PostForm} />
-                <Route exact path="/LogIn/SignUp" component={Login} />
-                <Route exact path="/Finance" component={Finance} />
-                <Route exact path="/Education" component={Education} />
-                <Route exact path="/SocialMedia" component={SocialMedia} />
-                <Route exact path="/Relationships" component={Relationships} />
-                <Route exact path="/Partying" component={Partying} />
-                <Route exact path="/Career" component={Career} />
-                <Route exact path="/TimeManagement" component={TimeManagement} />
-                <Route exact path="/Family" component={Family} />
-                <Route exact path="/userProfile/:id" component={UserProfile} />
-                <Route exact path="/topic/:id/posts" component={TopicPosts} /> */}
+import { Security, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
 
 
-            </Switch>
-        </div>
-    </Router>
+import React, { Fragment } from 'react';
+
+
+
+
+
+const App = ({ classes }) => (
+  <Fragment>
+    
+    <main className={classes.main}>
+      <Home />
+    </main>
+  </Fragment>
 );
 
+export default App;
 
 
-        // return (
-        //     <div className="mainContainer">
                 
 
 
-export default App;
--- server.js
+++ server.js
@@ -3,8 +3,37 @@ const bodyParser = require("body-parser");
 const Cors = require('cors');
 const logger = require('morgan');
 const routes = require("./routes");
+const OktaJwtVerifier = require('@okta/jwt-verifier');
 
-const app = express();
+const oktaJwtVerifier = new OktaJwtVerifier({
+    issuer: 'https://dev-336569.oktapreview.com/oauth2/default',
+    clientId: '0oaj1jfs16YgQs7LW0h7',
+    assertClaims: {
+      aud: 'api://default',
+    },
+  });
+
+  function authenticationRequired(req, res, next) {
+    const authHeader = req.headers.authorization || '';
+    const match = authHeader.match(/Bearer (.+)/);
+  
+    if (!match) {
+      return res.status(401).end();
+    }
+  
+    const accessToken = match[1];
+  
+    return oktaJwtVerifier.verifyAccessToken(accessToken)
+      .then((jwt) => {
+        req.jwt = jwt;
+        next();
+      })
+      .catch((err) => {
+        res.status(401).send(err.message);
+      });
+  }
+  
+  const app = express();
 // express/ API port
 const PORT = process.env.PORT || 3001;
 
@@ -18,6 +47,20 @@ app.use(logger('dev'));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+app.get('/secure', authenticationRequired, (req, res) => {
+    res.json(req.jwt);
+  });
+  
+  /**
+   * Another example route that requires a valid access token for authentication, and
+   * print some messages for the user if they are authenticated
+   */
+  app.get('/api/messages', authenticationRequired, (req, res) => {
+    res.json([{
+      message: 'Hello, world!'
+    }]);
+  });
 
 //test to make sure express is working
 app.get('/api/getList', (req,res) => {
