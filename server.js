10d7089c3f33b7a8b8bc3cdb370e01fc2078ef2e
nmiesmer <nic.miesmer@segmint.com>
2019-01-21 20:13:44 -0500

    un comment app.use routes ( I don't know why it worked)

--- server.js
+++ server.js
@@ -67,14 +67,9 @@ app.get('/secure', authenticationRequired, (req, res) => {
     }]);
   });
 
-//test to make sure express is working
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
-});
 
-// app.use(routes);
+
+app.use(routes);
 
 
 
