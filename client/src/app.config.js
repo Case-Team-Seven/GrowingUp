ca7d0dd116d3fae9c3b6680828a5087c674f59c3
nmiesmer <nic.miesmer@segmint.com>
2019-01-23 19:27:28 -0500

    replacing Okta to be it's normal sense

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,6 +1,6 @@
 export default {
     url: 'https://dev-336569.oktapreview.com',
     issuer: 'https://dev-336569.oktapreview.com/oauth2/default',
-    redirect_uri: 'https://grownupish.herokuapp.com/implicit/callback',
+    redirect_uri: window.location.origin + '/implicit/callback',
     client_id: '0oaj1jfs16YgQs7LW0h7'
   };
